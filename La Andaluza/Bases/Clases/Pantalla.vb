aeda18238bdd41ae0f366b78522829be588c9459
miguel_vega_menacho <miguel.vega.menacho@gmail.com>
2014-09-29 11:27:07 +0200

    no message

--- La Andaluza/Bases/Clases/Pantalla.vb
+++ La Andaluza/Bases/Clases/Pantalla.vb
@@ -213,8 +213,9 @@
 
     Shared Function mostrarDialogo(frm As Form) As DialogResult
         Dim glass As New glassPanel
+        glass.Text = frm.Text
         glass.Show()
-
+        glass.FormBorderStyle = FormBorderStyle.None
         Dim result As DialogResult = frm.ShowDialog
         glass.Close()
         Return result
